b535f0fc2739c3ad67e2167ff0ab715cdc1e81a9
cscroy <cciipq1@gamil.com>
2018-07-13 07:37:39 +0000

    asdf

--- config/database.php
+++ config/database.php
@@ -1,28 +1,5 @@
 <?php
 
-function get_db_config()
-{
-    if (getenv('IS_IN_HEROKU')) {
-        $url = parse_url(getenv("DATABASE_URL"));
-
-        return $db_config = [
-            'connection' => 'pgsql',
-            'host' => $url["host"],
-            'database'  => ltrim($url["path"], "/"),
-            'username'  => $url["user"],
-            'password'  => $url["pass"],
-            'port'  =>  $url["port"],
-        ];
-    } else {
-        return $db_config = [
-            'connection' => env('DB_CONNECTION', 'mysql'),
-            'host' => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
-        ];
-    }
-}
 $db_config = get_db_config();
 
 return [
@@ -39,7 +16,7 @@ return [
     */
 
 
-    'default' => $db_config['connection'],
+    'default' => 'pgsql',
 
     // 'default' => env('DB_CONNECTION', 'mysql'),
 
